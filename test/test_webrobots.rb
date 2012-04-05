3a16205ca565b354ed4ecdbf8b04fb0f28469d9a
Prakash Murthy <prakash.n.murthy@gmail.com>
2012-04-05 16:57:32 -0600

    Added a failing test for correctly parsing sitemaps when there are no spaces between user-agent definitions

--- test/test_webrobots.rb
+++ test/test_webrobots.rb
@@ -679,4 +679,48 @@ TXT
       assert_equal(["http://site1.example.com/text/sitemap.xml"], @robots.sitemaps("http://site1.example.com/"))
     end
   end
+
+  context "robots.txt with no space between user-agents" do
+    setup do
+      @robots = WebRobots.new('RandomBot', :http_get => lambda { |uri|
+        res = case uri.to_s
+          when 'http://site1.example.com/robots.txt'
+          <<-'TXT'
+User-agent: *
+Request-rate: 1/30
+Disallow: /util/
+
+User-agent: 007
+Disallow: /errors/
+
+Sitemap: http://site1.example.com/text/sitemap.xml
+Sitemap: http://site1.example.com/text/sitemap2.xml
+ 
+TXT
+        when 'http://site2.example.com/robots.txt'
+          <<-'TXT'
+User-agent: *
+Request-rate: 1/30
+Disallow: /util/
+User-agent: 007
+Disallow: /errors/
+
+Sitemap: http://site2.example.com/text/sitemap.xml 
+Sitemap: http://site2.example.com/text/sitemap2.xml 
+TXT
+        else
+          raise "#{uri} is not supposed to be fetched"
+        end
+        # This chomp is actually key to the test.  Remove the final EOL.
+        # The final line should be the one ending with the space.
+        res.chomp
+      })
+    end
+
+    should "be properly parsed" do
+      assert_equal( ["http://site1.example.com/text/sitemap.xml", "http://site1.example.com/text/sitemap2.xml" ] , @robots.sitemaps("http://site1.example.com/"))
+      assert_equal(["http://site2.example.com/text/sitemap.xml", "http://site2.example.com/text/sitemap2.xml"], @robots.sitemaps("http://site2.example.com/"))
+    end
+  end
+
 end
